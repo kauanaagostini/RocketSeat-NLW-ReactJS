63812e41ffbba199ffc48be513f850ab78ec81b8
Kauana Maria Agostini <kauanaagostini@gmail.com>
2021-04-24 18:58:39 -0300

    implementado o play dentro da pagina do episódio.

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -6,6 +6,7 @@ import { GetStaticPaths, GetStaticProps } from 'next';
 
 import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
+import { usePlayer } from '../../contexts/PlayerContext';
 
 import styles from './episode.module.scss';
 
@@ -26,6 +27,8 @@ type EpisodeProps = {
 }
 
 export default function Episode({ episode }: EpisodeProps) {
+  const { play } = usePlayer()
+
   return(
     <div className={styles.episode}>
       <div className={styles.thumbnailContainer}>
@@ -40,7 +43,7 @@ export default function Episode({ episode }: EpisodeProps) {
           src={episode.thumbnail}
           objectFit='cover'
         />
-        <button type="button">
+        <button type="button" onClick={() => play(episode)}>
           <img src="/play.svg" alt="Tocar episódio" />
         </button>
       </div>
